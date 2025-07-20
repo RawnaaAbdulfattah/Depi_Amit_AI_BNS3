fcfdd22e09fc7ad11018d1ccf9b525b84829a9a5
RawnaaAbdulfattah <rawnaashawki@email.com>
2025-07-20 20:07:09 +0300

    a

--- sources/session/code/ok.py
+++ sources/session/code/ok.py
@@ -1,3 +1,4 @@
+"""
 while True:
         print("\nWelcome to the Simple Calculator!")
         print("Select an operation:")
@@ -49,3 +50,29 @@ while True:
                 print(f"{n1}/{n2} = ",c1.div())
             except:
                     print("Error:dividing by zero")
+                """
+    
+
+import os
+import random
+
+def handle_directory(base_path: str):
+   
+    if not os.path.exists(base_path):
+        os.makedirs(base_path)
+
+    folder_paths = [] 
+    for i in range(10):
+        inner_path = os.path.join(base_path, f"dir_{i}")
+        os.makedirs(inner_path, exist_ok=True)
+        folder_paths.append(inner_path)
+        print(len(folder_paths))
+    random_dir=random.choice(folder_paths)
+    print(random_dir)
+    for i in range(5):
+        print("deleted:",random_dir)
+        os.rmdir(random_dir)
+        folder_paths.remove(random_dir)
+base_path = "C:/Users/Computer Market/Desktop/Depi/Depi_Amit_AI_BNS3/sources/session/code/directories"
+handle_directory(base_path)
+     
